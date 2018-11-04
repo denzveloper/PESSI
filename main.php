dd937d1a63eb149dd689eba245e6ebdc60a02b7f
DENZVELOPER - DzEN/DzEN <denzveloper@users.noreply.github.com>
2018-11-04 23:20:26 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,25 @@
+function encrypt($string, $ky='1234'){
+		$str=strval($string);
+		$ky=str_replace(chr(183),'',$ky);
+		$kl=strlen($ky)<32?strlen($ky):32;
+		$k=array();for($i=0;$i<$kl;$i++){
+		$k[$i]=ord($ky{$i})&0x1F;}
+		$j=0;for($i=0;$i<strlen($str);$i++){
+		$e=ord($str{$i});
+		$str{$i}=$e&0xE0?chr($e^$k[$j]):chr($e);
+		$j++;$j=$j==$kl?0:$j;}
+		return base64_encode(addslashes($str));
+}
+    
+function dencrypt($string, $ky='1234'){
+		$str=strval(stripslashes(base64_decode($string)));
+		$ky=str_replace(chr(183),'',$ky);
+		$kl=strlen($ky)<32?strlen($ky):32;
+		$k=array();for($i=0;$i<$kl;$i++){
+		$k[$i]=ord($ky{$i})&0x1F;}
+		$j=0;for($i=0;$i<strlen($str);$i++){
+		$e=ord($str{$i});
+		$str{$i}=$e&0xE0?chr($e^$k[$j]):chr($e);
+		$j++;$j=$j==$kl?0:$j;}
+		return $str;
+	}
